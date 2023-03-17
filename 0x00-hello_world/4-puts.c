10bfebc0fdba27a6988e15c03b4e3780adbcc21c
ridachokairi <chokairi.rida@gmail.com>
2023-03-17 13:27:12 -0700

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 /**
- * main -prints "Programming is like building a multilingual puzzle"
+ * main prints "Programming is like building a multilingual puzzle"
  * return 0;
  */
 int main(void)
