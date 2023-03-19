20e1fb9e489c11d2bf18f555cc33d8ee21e263fc
ridachokairi <chokairi.rida@gmail.com>
2023-03-18 17:00:53 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
-#include<stdio.h>
-/*
- * main prints "with proper grammar, but the outcome is a piece of art"
- * return 0;
+#include <stdio.h>
+/**
+ * main - Prints "with proper grammar, but the outcome is a piece of art"
  *
+ * return: Always 0 (Success)
  */
 int main(void)
 {
