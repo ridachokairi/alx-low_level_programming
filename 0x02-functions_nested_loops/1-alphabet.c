d19783724887baa5157acb05ce34c5722a129561
ridachokairi <chokairi.rida@gmail.com>
2023-03-21 15:44:52 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * print_ alphabets - print alphabets
+ * print_alphabet - print alphabets
  */
 
 void print_alphabet(void)
 {
 	char i;
 
-	for(i = 'a' ; i <= 'z' ; i++)
+	for (i = 'a' ; i <= 'z' ; i++)
 		_putchar(i);
 	_putchar('\n');
 }
