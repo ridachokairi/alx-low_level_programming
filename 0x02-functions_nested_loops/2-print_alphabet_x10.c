df1ee01b3b3948cf233367c417db4ca1ff74ad34
ridachokairi <chokairi.rida@gmail.com>
2023-03-21 16:01:45 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,9 +10,9 @@ void print_alphabet_x10(void)
 	int i;
 	char j;
 
-	for ( i = 0 ; i <= 9 ; i++)
+	for (i = 0 ; i <= 9 ; i++)
 	{
-		for ( j = 'a' ; j <= 'z' ; j++)
+		for (j = 'a' ; j <= 'z' ; j++)
 			_putchar(j);
 		_putchar('\n');
 	}
