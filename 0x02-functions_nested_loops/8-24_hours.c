58d211ff7d97b9c493fa3bc287a96804394f83c7
ridachokairi <chokairi.rida@gmail.com>
2023-03-22 12:37:01 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,9 +10,9 @@ void jack_bauer(void)
 	int i;
 	int j;
 
-	for(i = 0; i <= 23 ; i++)
+	for (i = 0; i <= 23 ; i++)
 	{
-		for(j = 0; j <= 59 ; j++)
+		for (j = 0; j <= 59 ; j++)
 		{
 			_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
