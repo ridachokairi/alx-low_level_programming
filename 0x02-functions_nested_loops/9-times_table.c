a5da316f967c5cab72507705fb555657801f5b78
ridachokairi <chokairi.rida@gmail.com>
2023-03-22 12:46:12 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,11 +9,13 @@ void times_table(void)
 {
 	int i;
 	int j;
+	int r;
 	for (i = 0 ; i <= 9 ; i++)
 	{
 		for (j = 0 ; j <= 9 ; j++)
 		{
-			_putchar(i * j);
+			r = i * j;
+			_putchar(r + ',');
 		}
 		_putchar('\n');
 	}
