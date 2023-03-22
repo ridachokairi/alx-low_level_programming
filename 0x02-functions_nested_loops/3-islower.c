180fce7fd824e48bda872a7c6e34094c61064973
ridachokairi <chokairi.rida@gmail.com>
2023-03-22 14:19:00 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * _islower - returns 1 if the character c is lowercase and
- * returns 0 if it is not the case
+ * _islower - Entry point
+ *  returns: 1 if the character c is lowercase and
+ * returns: 0 if it is not the case
  * @c : character
  */
 
