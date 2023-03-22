98003d544ee303886671d96fe2662c84e9d89922
ridachokairi <chokairi.rida@gmail.com>
2023-03-22 13:53:02 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,7 +15,7 @@ void print_to_98(int n)
 		for (i = n; i < 98; i++)
 			printf("%d, ", i);
 	else
-		for (i = n; n > 98; i--)
+		for (i = n; n > 98; --i)
 			printf("%d, ", i);
 	printf("98\n");
 }
