228aff6c7feb72ca2554649ea6038783808de258
ridachokairi <chokairi.rida@gmail.com>
2023-03-22 12:43:34 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times tables
+ *
+ */
+
+void times_table(void)
+{
+	int i;
+	int j;
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		for (j = 0 ; j <= 9 ; j++)
+		{
+			_putchar(i * j);
+		}
+		_putchar('\n');
+	}
+}
