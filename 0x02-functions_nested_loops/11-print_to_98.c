2aaaeb20dee125118f9f67110caf477cfa1dd13d
ridachokairi <chokairi.rida@gmail.com>
2023-03-22 13:50:16 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,7 +4,7 @@
 /**
  * print_to_98 - prints the number of a given number n to 98 
  * in order
- *
+ *@n:integer
  */
 
 void print_to_98(int n)
@@ -12,10 +12,10 @@ void print_to_98(int n)
 	int i;
 
 	if (n < 98)
-		for (i = n ; i < 98 ; i++)
+		for (i = n; i < 98; i++)
 			printf("%d, ", i);
 	else
-		for (i = n ; n > 98 ; i--)
+		for (i = n; n > 98; i--)
 			printf("%d, ", i);
 	printf("98\n");
 }
