f3365a41af846d66043d57c8df548716ab46f267
ridachokairi <chokairi.rida@gmail.com>
2023-03-22 17:35:43 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,10 +11,10 @@ void print_times_table(int n)
 
 	if (n >= 0 && n <= 15)
 	{
-		for (a = 0; a <= n; a++)
+		for (a = 0 ; a <= n ; a++)
 		{
 			_putchar('0');
-			for (b = 1; b <= n; b++)
+			for (b = 1 ; b <= n ; b++)
 			{
 				_putchar(',');
 				_putchar(' ');
