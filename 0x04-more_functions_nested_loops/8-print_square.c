c130a03f02468e2410f1d57f9234ef6666c176dc
ridachokairi <chokairi.rida@gmail.com>
2023-03-23 15:56:59 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -14,9 +14,9 @@ void print_square(int size)
 		_putchar('\n');
 	else
 	{
-		for (i = 0; i < size; i++)
+		for (i = 1; i <= size; i++)
 		{
-			for (j = 0; j < size ;j++)
+			for (j = 1; j <= size ; j++)
 			{
 				_putchar(35);
 			}
