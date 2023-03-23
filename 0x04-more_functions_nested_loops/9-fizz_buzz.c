e6aab1e9ae120ebefb2922502935ff8bccec6e7b
ridachokairi <chokairi.rida@gmail.com>
2023-03-23 16:22:38 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,13 +13,14 @@ int main(void)
 
 	for (i = 0; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz ");
+		if (i % 3 != 0 && i % 5 != 0)
+			printf("%d ", i);
 		else if (i % 3 == 0)
-			printf("Fizz ");
-		else if (i % 5 == 0)
-			printf("Buzz ");
+			if (i % 5 == 0)
+				printf("FizzBuzz ");
+			else
+				printf("Fizz ");
 		else
-			printf("%d ",i);
+			printf("Buzz ");
 	}
 }
