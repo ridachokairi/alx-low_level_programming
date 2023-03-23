0e29eb3e9efd0cd073d26d146385cb4bdbe994ef
ridachokairi <chokairi.rida@gmail.com>
2023-03-23 14:34:00 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,15 +8,17 @@
 
 void more_numbers(void)
 {
-	int i,j;
+	int i;
+	char j;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			_putchar(j);
+			if (j > 9)
+				_putchar('1');
+			_putchar((j % 10) +'0');
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
