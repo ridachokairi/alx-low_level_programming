4e2926169ca8c655127141deca74982048e225c1
ridachokairi <chokairi.rida@gmail.com>
2023-03-27 15:12:22 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string 
+ *
+ */
+
+int _strlen(char *s)
+{
+	int len,i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	len = i;
+	return (len);
+}
