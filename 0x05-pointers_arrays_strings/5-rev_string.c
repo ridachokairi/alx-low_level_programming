cfad10f3a61b338147a295ec32060ffbf779c698
ridachokairi <chokairi.rida@gmail.com>
2023-03-27 16:58:51 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,14 +8,14 @@
 
 void rev_string(char *s)
 {
-	int j = 0,i;
+	int j = 0, i;
 	char str = s[0];
 
 	while (s[j] != '\0')
 	{
 		j++;
 	}
-	for (i = 0; i < j ;i++)
+	for (i = 0; i < j ; i++)
 	{
 		j--;
 		str = s[i];
