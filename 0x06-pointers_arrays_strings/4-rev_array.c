bbe5d7a1524befa108e3eee1a536285cc1e3900b
ridachokairi <chokairi.rida@gmail.com>
2023-03-30 19:28:27 -0700

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,24 +10,12 @@
 void reverse_array(int *a, int n)
 {
 	int b;
-	int i ;
-
-	if (n % 2 == 0)
+	int i;
+	
+	for (i = 0 , i < n--, i++)
 	{
-		for (i = 0 , i < (n - 1) / 2, i++)
-		{
-			b = a[i];
-			a[i] = a[n - 1 - i];
-			a[n - 1 - i] = b;
-		}
-	}
-	else
-	{
-		for (i = 0 , i < n / 2 , i++)
-		{
-			b = a[i];
-			a[i] = a[n - 1 - i];
-			a[n - 1 - i] = b;
-		}
+		b = a[i];
+		a[i] = a[n];
+		a[n] = b;
 	}
 }
