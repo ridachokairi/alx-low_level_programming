ed91c448e3bc2cab3503cb27d00ced01227c66a6
ridachokairi <chokairi.rida@gmail.com>
2023-04-03 14:48:23 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _mempcy - copies memory area
+ * @dest : the memory area to copy to 
+ * @src: the memory area to copy from
+ * @n: number of bytes to copy
+ * Return: pointer
+ */
+
+char *_mempcy(char *dest, char *src, unsigned int n)
+{
+	int i = 0;
+
+	for (; n > 0 ; i++)
+	{
+		dest[i] = src[i];
+		n--;
+	}
+	return (dest);
+}
