aa676b423c031ac47c6444f52311ae6f7b2821e3
ridachokairi <chokairi.rida@gmail.com>
2023-04-03 14:54:37 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,10 +12,9 @@ char *_mempcy(char *dest, char *src, unsigned int n)
 {
 	int i = 0;
 
-	for (; n > 0 ; i++)
+	for (; n > 0; n--)
 	{
-		dest[i] = src[i];
-		n--;
+		dest[n] = src[n];
 	}
 	return (dest);
 }
