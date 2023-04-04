f6d83f96711aacd6afc4a2d3060e9ac20666d096
ridachokairi <chokairi.rida@gmail.com>
2023-04-03 17:00:06 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,7 +4,7 @@
  * _strchr - locates a character in a string
  * @s: a pointer to a string
  * @c: the character we look for
- * Return: the first occurence of the character c in the string s, 0 if the character is not found
+ * Return: pointer to the first occurence of the character c in the string s, 0 if the character is not found
  *
  */
 
@@ -15,7 +15,7 @@ char *_strchr(char *s, char c)
 	for (i = 0; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
-			return (&s[i]);
+			return (s + i);
 	}
-	return (0);
+	return ('\0');
 }
