c77bd627402950c9adf4dd9e7d017b4bc940cd5b
ridachokairi <chokairi.rida@gmail.com>
2023-04-05 15:52:54 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,25 +1,10 @@
 #include "main.h"
 
 /**
- * is_palindrome - checks if a string is a palindrome
- * @s: string
- * Return: 1 if s is palindrome, 0 otherwise
- *
- */
-
-int is_palindrome(char *s)
-{
-	int i = _strlen_recursion(s);
-	return (check_palindrome(s, 0, i - 1));
-}
-
-/**
  * _strlen_recursion - returns the length of string
  * @s: string
  * Return: integer
- *
  */
-
 int _strlen_recursion(char *s)
 {
 	if (*s != '\0')
@@ -28,12 +13,11 @@ int _strlen_recursion(char *s)
 }
 
 /**
- * check_palindrome - checks a string is a palindrome
+ * check_palindrome - palindrome
  * @s: string
  * @l: left index
  * @r: right index
  * Return: integer
- *
  */
 
 int check_palindrome(char *s, int l, int r)
@@ -43,5 +27,19 @@ int check_palindrome(char *s, int l, int r)
 			return (1);
 		else
 			return (check_palindrome(s, l + 1, r - 1));
-	return (0);
+	else
+		return (0);
+}
+
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string
+ * Return: 1 if s is palindrome, 0 otherwise
+ *
+ */
+
+int is_palindrome(char *s)
+{
+        int i = _strlen_recursion(s);
+        return (check_palindrome(s, 0, i - 1));
 }
