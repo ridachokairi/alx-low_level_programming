718dea654f4b9de5d79d711c6cfabd14156bebc2
ridachokairi <chokairi.rida@gmail.com>
2023-04-08 16:48:50 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -22,25 +22,15 @@ int main(int argc, char* argv[])
 		while (atoi(argv[1]) > 0)
 		{
 			if (atoi(argv[1]) >= 25)
-			{
 				atoi(argv[1]) = atoi(argv[1]) - 25;
-			}
 			else if (atoi(argv[1]) >= 10)
-			{
 				atoi(argv[1]) = atoi(argv[1]) - 10;
-			}
 			else if (atoi(argv[1]) >= 5)
-			{
 				atoi(argv[1]) = atoi(argv[1]) - 5;
-			}
 			else if (atoi(argv[1]) >= 2)
-			{
 				atoi(argv[1]) = atoi(argv[1]) - 2;
-			}
 			else if (atoi(argv[1]) >= 1)
-			{
 				atoi(argv[1]) -= 1;
-			}
 			cent += 1;
 		}
 	}
