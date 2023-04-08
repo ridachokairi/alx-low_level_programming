2e355f0a4a4f728b32e925ca3ab1899a7fd2c92c
ridachokairi <chokairi.rida@gmail.com>
2023-04-08 10:14:21 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,4 +2,21 @@
 #include <stdio.h>
 
 /**
+ * print-number_arguments - prints the number of arguments
+ * followed by a new line
+ * @argc: arguments count
+ * @argv: array of strings
  *
+ */
+
+void print_number_arguments(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (argv[i])
+	{
+		i++;
+	}
+	printf("%d",i);
+	_putchar('\n');
+}
