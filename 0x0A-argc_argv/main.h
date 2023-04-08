da53d998ff032b7aee9a89ff1131cef873e5fe77
ridachokairi <chokairi.rida@gmail.com>
2023-04-08 09:54:17 -0700

    header file

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void print_program_name(int argc, char *argv[]);
+void print_number_arguments(int argc, char *argv[]);
+void print_all_arguments(int argc, char *argv[]);
+int multiplication(int argc, char *argv[], int a, int b);
+int add_positive_numbers(int argc, char *argv[], int a, int b);
+
+#endif /* MAIN_H */
