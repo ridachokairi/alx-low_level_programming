bfaeb3773a2f7d17feee555dc99b7b92a885f244
ridachokairi <chokairi.rida@gmail.com>
2023-05-01 08:40:16 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a singly linked list
+ * @head: head of a singly linked list
+ * Return: NULL
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *node, *next_node;
+
+	if (!head)
+		return;
+	node = *head;
+	while (node)
+	{
+		node = node->next;
+		free(node);
+		next_node = node->next;
+	}
+	*head = NULL;
+}
